3e6a21f0b51df73c2268b23cbe333b6a77162a2e
Sayhan Ahmed <sayanahmed228@gmail.com>
2025-11-08 21:13:28 +0600

    Setup the server

---
+++ index.js
@@ -0,0 +1,46 @@
+// utility-bill
+// NeccX94TI5jxD1f2
+
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://utility-bill:NeccX94TI5jxD1f2@cluster0.eoqyygq.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Utility Bill server is running");
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Utility Bill server is running on ${port}`);
+});
